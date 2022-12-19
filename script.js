3e7db4e9dde6cce876e1a6cc6ada924656eb0e9a
Loïc Lecointe <loic.lecointe.etu>
2022-12-19 13:06:24 +0100

    Modification text and images

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Etudiant", "en", "Informatique"],
+        strings: ["etudiant", "en", "informatique"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true,
     });
 
     var typed1 = new Typed(".typing-2", {
-        strings: ["Etudiant", "en", "Informatique"],
+        strings: ["etudiant", "en", "informatique"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true,
